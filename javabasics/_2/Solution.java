b6feba83fb940f15a8fa162779adcb7d136cb235
Gabriele <gabriele.castiglione@libero.it>
2023-07-19 17:42:56 +0200

    solution exercise 2

---
+++ javabasics/_2/Solution.java
@@ -0,0 +1,27 @@
+package javabasics._2;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+    public static void exercise1() {
+        System.out.println("Exercise 1: ");
+        System.out.print("Hello Mario!");
+    }
+    public static void exercise2() {
+        System.out.println("\nExercise 2: ");
+        System.out.print("Hello");
+        System.out.print(" Mario!");
+
+    }
+    public static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        System.out.print("This should ");
+        System.out.print("all appear ");
+        System.out.print("on the same line");
+
+    }
+}
