bf91b2c4924d42f2f39a60c9c4d383910199c461
Gabriele <gabriele.castiglione@libero.it>
2023-07-20 16:57:33 +0200

    Update Main exercise 4 and add Solution exercise 4

--- javabasics/_4/Solution.java
+++ javabasics/_4/Solution.java
@@ -1,6 +1,7 @@
 package javabasics._4;
 
-public class Main {
+public class Solution {
+
     public static void main(String[] args) {
         exercise1();
         exercise2();
@@ -13,7 +14,8 @@ public class Main {
      */
     private static void exercise1() {
         System.out.println("Exercise 1:");
-        // Write your code here
+        int age = 28;
+        System.out.println("My age is: " + age);
     }
 
     /**
@@ -25,19 +27,21 @@ public class Main {
      */
     private static void exercise2() {
         System.out.println("\nExercise 2:");
-        //Write your code here
+        char firstInitials = 'G';
+        int age = 28;
+        System.out.println("My age is: " + age + ",my initial=" + firstInitials);
     }
 
     /**
      * 3: Write comments above each line of code, describing what the line of code is doing
      */
     private static void exercise3() {
-        System.out.println("\nExercise 3:");
-        boolean hasEatenLunch = false;
+        System.out.println("\nExercise 3:");       // Print a new line and the text "Exercise 3:"
+        boolean hasEatenLunch = false;      // Declare and initialize a boolean variable "hasEatenLunch" with the value "false"
 
-        double lunchCost = 5.99;
+        double lunchCost = 5.99;    // Declare and initialize a Double variable "lunchCost" with the value "5.99"
 
-        System.out.println("Lunch cost=" + lunchCost);
-        System.out.println("Has Eaten lunch=" + hasEatenLunch);
+        System.out.println("Lunch cost=" + lunchCost);      // Print the text "Lunch cost=" followed by the value of the variable "lunchCost".
+        System.out.println("Has Eaten lunch=" + hasEatenLunch);     // Print the text "Has Eaten lunch=" followed by the value of the variable "hasEatenLunch".
     }
 }
